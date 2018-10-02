7189b8875004a37ce5bac2f03a65c94b313d3c12
aaronluannguyen <aaronluannguyen@gmail.com>
2018-10-02 00:49:09 -0700

    Passed all tests

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,66 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        let possibleFunctionWord:String = args[args.count - 1]
+        let arrayLength:Int = Int(args.count)
+        switch possibleFunctionWord {
+        case "count":
+            if arrayLength == 1 {
+                return 0
+            }
+            return arrayLength - 1
+            
+        case "avg":
+            if arrayLength == 1 {
+                return 0
+            }
+            var sum = 0
+            for i in 0..<(args.count - 1) {
+                let num:Int = Int(args[i])!
+                sum = sum + num
+            }
+            let result = sum / (arrayLength - 1)
+            return result
+            
+        case "fact":
+            if arrayLength == 1 {
+                return 0
+            }
+            let num:Int = Int(args[0])!
+            if num == 0 {
+                return 1
+            }
+            var answer = 1
+            for i in 1...num {
+                answer = i * answer
+            }
+            return answer
+            
+        default:
+            let n1:Int = Int(args[0])!
+            let n2:Int = Int(args[2])!
+            let functionSign:String = args[1]
+            switch functionSign {
+            case "+":
+                return Int(n1 + n2)
+                
+            case "-":
+                return Int(n1 - n2)
+                
+            case "/":
+                return Int(n1 / n2)
+                
+            case "%":
+                return Int(n1 - ((n1 / n2) * n2))
+                
+            case "*":
+                return Int(n1 * n2)
+                
+            default:
+                return 0
+            }
+        }
+    
     }
     
     public func calculate(_ arg: String) -> Int {
@@ -23,4 +83,3 @@ let first = readLine()!
 let operation = readLine()!
 let second = readLine()!
 print(Calculator().calculate([first, operation, second]))
-
